d2dd551b122c8fa59ff8b2d719208aa34fe1e015
Kaiqin Huang <huang214@mail.chapman.edu>
2016-10-28 09:13:01 +0000

    edit

--- src/factorial.c
+++ src/factorial.c
@@ -3,10 +3,10 @@
 /* Definition for "factorial"
  * Note: this must match the type specified in the header file above
  */
+
 int factorial(int n) {
     /* By convention in C, all variables must by type defined at the top
      * of each function */
-	
 	int fac;
 	if (n >= 0){
 		fac = 1; 
