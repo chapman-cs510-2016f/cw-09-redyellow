7676cd2159a58782f5380d894e2c23bafb5615b9
Kaiqin Huang <huang214@mail.chapman.edu>
2016-10-28 09:13:36 +0000

    edit

--- src/test_factorial.c
+++ src/test_factorial.c
@@ -6,7 +6,6 @@ int main(int argc, char **argv) {
 	return 0;
 }
 
-
 void print_factorial() {
 	printf("Factorial of 100 is: %d\n", factorial(100));
 	printf("Factorial of 0 is: %d\n", factorial(0));
